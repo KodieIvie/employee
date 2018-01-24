9c813100c73910ecd49157d29f3277baabd1dff3
kodie <fxit85@users.noreply.github.com>
2018-01-24 16:09:05 -0700

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const app = express();
 
-const port = ENV.port || 3000;
+const port = process.env.PORT || 3000;
 // use sessions for tracking logins
 app.use(session({
   secret: 'something secret',
